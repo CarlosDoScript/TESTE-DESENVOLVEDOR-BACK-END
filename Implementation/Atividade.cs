53bb68b1b18a1069f0926f8acdca660246c720c8
Carlos Henrique <carlosmvsep3@hotmail.com>
2024-02-02 21:10:52 -0300

    Add project files.

---
+++ Implementation/Atividade.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATIVIDADE_1.Implementation
+{
+    public class Atividade
+    {
+        //ATIVIDADE 1.1
+        private static object lockObj = new object();
+        public static void Atividade_1()
+        {
+            int maiorSequencia = 0, maiorNumeroInicial = 0;
+            Dictionary<int, int> sequencias = new Dictionary<int, int>();
+
+            for (int numeroInicial = 1; numeroInicial <= 1000001; numeroInicial++)
+            {
+                int sequenciaAtual = CalcularSequenciaCollatz(numeroInicial, sequencias);
+                lock (lockObj)
+                {
+                    if (sequenciaAtual > maiorSequencia)
+                    {
+                        maiorSequencia = sequenciaAtual;
+                        maiorNumeroInicial = numeroInicial;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Maior número inicial: {maiorNumeroInicial}");
+            Console.WriteLine($"Tamanho da sequência: {maiorSequencia}");
+        }
+        
+        //função que calcula o tamanho da sequencia de collatz para um numero inicial
+        static int CalcularSequenciaCollatz(int numeroInicial, Dictionary<int, int> sequencias, int profundidade = 0, HashSet<int> visitados = null)
+        {
+            if (visitados == null)
+                visitados = new HashSet<int>();
+
+            if (visitados.Contains(numeroInicial))
+                return 1;
+
+            visitados.Add(numeroInicial);
+
+            if (numeroInicial == 1 || profundidade > 10000) 
+                return 1;
+
+            if (sequencias.ContainsKey(numeroInicial))
+                return sequencias[numeroInicial];
+
+            int proximoNumero = numeroInicial % 2 == 0 ? numeroInicial >> 1 : (numeroInicial << 1) + numeroInicial + 1;
+            int sequenciaAtual = 1 + CalcularSequenciaCollatz(proximoNumero, sequencias, profundidade + 1, visitados);
+            sequencias[numeroInicial] = sequenciaAtual;
+
+            return sequenciaAtual;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ATIVIDADE_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Inicia();
+        }
+
+        public static void Inicia()
+        {
+            int atividadeEscolha = 0;
+            bool entradaValida = false;
+
+            Console.WriteLine("ESCOLHA UMA ATIVIDADE PARA EXECUTAR: ");
+
+            while (!entradaValida)
+            {
+                Console.WriteLine("----------------------------------------------");
+                Console.WriteLine("1 - ATIVIDADE(CONJECTURA DE COLLATZ) 1.1");
+                Console.WriteLine("2 - ATIVIDADE(SOMENTE NUMEROS IMPARES) 1.2");
+                Console.WriteLine("3 - ATIVIDADE 1.3");
+                Console.WriteLine("----------------------------------------------");
+
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out atividadeEscolha))
+                {
+                    if (atividadeEscolha >= 1 && atividadeEscolha <= 3)
+                    {
+                        entradaValida = true;
+
+                        switch (atividadeEscolha)
+                        {
+                            case 1:
+
+                                Implementation.Atividade.Atividade_1();
+
+                                Console.WriteLine("----------------------------------------------");
+
+                                Inicia();
+
+                                break;
+
+                            case 2:
+
+                                break;
+
+                            case 3:
+
+                                break;
+
+                            default:
+                                Console.WriteLine("Opção inválida, Digite um número entre 1 e 3.");
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Número inválido. Digite um número entre 1 e 3: ");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Entrada inválida. Digite um número entre 1 e 3: ");
+                }
+            }
+
+        }
+    }
+}
